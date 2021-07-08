3d4bf384aa77f2199c64bed191ce05727cf79cd1
simbasang <sebastian.englund@live.se>
2021-07-08 21:59:29 +0200

    init

---
+++ dayFive/dayFive/dayFive/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace dayFive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passports = File.ReadAllText(@"./input.txt").Split("\r\n").ToList();
+            var highestNumbre = 0;
+            //foreach (var passport in passports)
+            //{
+            //    var temp = Search(passport);
+            //    highestNumbre = temp > highestNumbre
+            //          ? temp
+            //          : highestNumbre;
+            //}
+            Console.WriteLine(Search("BBFFBBFRLL"));
+            Console.ReadLine();
+        }
+
+        public static int Search(string bordingPass)
+        {
+            var columnInput = bordingPass.Substring(bordingPass.Length - 3);
+            var rowInput = bordingPass.Replace(columnInput, "");
+            var row = GetRow(127, rowInput);
+            var column = GetRow(7, columnInput);
+            return row * 8 + column;
+        }
+
+
+       //denna funkar inte på en lista som är jämn
+        public static int GetRow(int max, string input)
+        {
+            var min = 0;
+            var mid = 0;
+            foreach (var ch in input)
+            {
+                mid = (min + max) / 2;
+
+                if (ch == 'F' || ch == 'L')
+                    max = mid - 1;
+
+                if (ch == 'B' || ch == 'R')
+                    min = mid + 1;
+            }
+            return mid;//(int)Math.Round(mid, MidpointRounding.AwayFromZero);
+        }
+
+        public static int DoWork(List<string> input)
+        {
+            int highestNumber = 0;
+            var result = 0;
+            foreach (var item in input)
+            {
+                var row = 127;
+                var column = 7;
+                foreach (var ch in item)
+                {
+                    var mid = row / 2;
+                    if (ch == 'F')
+                    {
+                        //lower
+
+                    }
+                    if (ch == 'B')
+                    {
+                        //higher
+                    }
+
+                }
+
+                if (result > highestNumber)
+                    highestNumber = result;
+            }
+
+            return highestNumber;
+        }
+
+
+    }
+}
---
+++ dayFour/dayFour/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace dayFour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var validPassports = File.ReadAllText(@"./input.txt")
+                .Split("\r\n\r\n")
+                .Where(
+                    x => x.Contains("byr")
+                    && x.Contains("iyr")
+                    && x.Contains("eyr")
+                    && x.Contains("hgt")
+                    && x.Contains("hcl")
+                    && x.Contains("ecl")
+                    && x.Contains("pid"))
+                .Select(x => x.Replace("\r\n", " ").Split(' ')).ToList(); ;
+
+            Console.WriteLine(checkPassportCridentials(validPassports));
+            Console.ReadLine();
+        }
+
+        public static int checkPassportCridentials(List<string[]> passportCridentials)
+        {
+
+            return passportCridentials.Where(x =>
+                 int.TryParse(x.FirstOrDefault(x => x.Contains("byr")).Replace("byr:", ""), out int byr) && byr >= 1920 && byr <= 2002
+                 && int.TryParse(x.FirstOrDefault(x => x.Contains("iyr")).Replace("iyr:", ""), out int iyr) && iyr >= 2010 && iyr <= 2020
+                 && int.TryParse(x.FirstOrDefault(x => x.Contains("eyr")).Replace("eyr:", ""), out int eyr) && eyr >= 2020 && eyr <= 2030
+                 && IsValidHight(x.FirstOrDefault(x => x.Contains("hgt")))
+                 && IsValidHailColor(x.FirstOrDefault(x => x.Contains("hcl")))
+                 && IsValidEyeColor(x.FirstOrDefault(x => x.Contains("ecl")))
+                 && IsValidPassportId(x.FirstOrDefault(x => x.Contains("pid"))))
+                .Count();
+        }
+
+        public static bool IsValidPassportId(string pid)
+        {
+            var value = pid.Split(':')[1];
+            return value.Length == 9 && Regex.IsMatch(value, @"\A\b[0-9]+\b\Z");
+        }
+
+        public static bool IsValidEyeColor(string ecl)
+        {
+            var validColor = new List<string> { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
+            return validColor.Contains(ecl.Split(':')[1]);
+        }
+
+        public static bool IsValidHailColor(string hcl)
+        {
+            var value = hcl.Split(':')[1];
+            return value.Length != 7 || value.First() != '#' || !Regex.IsMatch(value.Replace("#", ""), @"\A\b[0-9a-fA-F]+\b\Z")
+                ? false
+                : true;
+        }
+
+        public static bool IsValidHight(string hgt)
+        {
+            var value = hgt.Split(':')[1];
+
+            if (value.Length < 4)
+                return false;
+
+            var heightType = value.Substring(value.Length - 2, 2);
+
+            if (!int.TryParse(value.Replace(heightType, ""), out int hight))
+                return false;
+
+            if (heightType == "cm" && hight <= 193 && hight >= 150)
+                return true;
+
+            if (heightType == "in" && hight <= 76 && hight >= 59)
+                return true;
+
+            return false;
+        }
+
+    }
+}
+
